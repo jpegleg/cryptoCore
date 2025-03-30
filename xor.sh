e50701899cf39e737779cf21cc5b9417590e6651
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-03-30 19:28:18 -0400

    Update xor.sh
    add note about educational context and add comment link to dwarven toolbox hexor

--- xor.sh
+++ xor.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 # Bash is not meant for this kind of thing.
-# Use python instead, for the big number
-# crunching. But for smaller numbers, or if
-# if you don't have access to another method,
-# you can do this in the shell. -Keegan 
+# Use rust or python instead, for the normal
+# crunching. But for some conceptual learnering 
+# with smaller numbers, or if you don't have access to another method,
+# you can do this in the shell. This is mostly
+# for educational purposes, for real cryptography
+# we use _bytes_ not _strings_. 
+
+# See https://github.com/jpegleg/dwarven-toolbox/blob/main/src/hexor.rs for XOR of bytes instead of strings.
+
 ###################################################
 #---->                                            #------->
 #--------->        CRYPTO CORE [ Bitwise XOR ]    # --->
@@ -14,7 +19,8 @@ tr -d '\040\011\012\015'
 }
 
 # Both your msg and your key seed files must
-# be binary and of the same length.
+# be binary strings of 0 an 1 characters
+# and of the same length.
 # Place them in the ~/seeds directory.
 # Your seed files must be named a.xor and b.xor.
 
